91c409b8e8fe187fc876252f8580f68b6da454e5
Carlos Alberto <carlos@spcbrasil.org.br>
2013-07-09 21:41:34 -0300

    primeiro comit da classe Fatorial

---
+++ Fatorial.java
@@ -0,0 +1,15 @@
+public class Fatorial { 
+
+       public static void main (String arg[]){
+
+             System.out.println(fatorial()+" 2000 ");
+
+       }
+
+       public static int fatorial(){
+     
+         return 17;
+
+       }
+
+}
